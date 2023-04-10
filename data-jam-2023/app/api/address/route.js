bf4e8e445c31ba6d6fc213f4086e93686f3c3905
quansummit <114690045+quansummit@users.noreply.github.com>
2023-04-10 10:46:30 -0400

    fetch default address

---
+++ data-jam-2023/app/api/address/route.js
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import { getUsers } from "@/lib/prisma/users";
+import { User } from "@prisma/client";
+import { NextResponse } from "next/server";
+import axios from "axios"; 
+
+export async function GET(request){
+    const {searchParams}= new URL(request.url);
+    try{
+        const baseURL= "https://geocoding.geo.census.gov/geocoder/locations/address"
+        const street = searchParams.get("street")
+        const city= searchParams.get("city")
+        const state = searchParams.get("state")
+        const zip = searchParams.get("zip")
+        const benchmark = searchParams.get("benchmark")
+        const format = searchParams.get("format")
+      const data = await axios.get(baseURL, {
+        params:{
+          street, city, state, zip, benchmark, format
+        }
+      })
+      const result = data.data.result;
+        const error = data.error
+        if(error) throw new Error(error)
+        return NextResponse.json({result}, {status:200})
+    }catch(error){
+        console.log("errored")
+        return NextResponse.json({error: error.message}, {status: 500})
+    }
+}
--- data-jam-2023/app/page.tsx
+++ data-jam-2023/app/page.tsx
@@ -1,22 +1,93 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import {useState} from 'react';
+import axios from "axios"
+import { 
+  AppBar, 
+  Paper, 
+  Typography, 
+  Box, 
+  TextField, 
+  InputAdornment, 
+  IconButton
+ } from '@mui/material';
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
+import Map from 'react-map-gl';
 
 
 export default function Home() {
+  
+  const [address, setAddress] = useState("");
+  const [addressData, setAddressData]= useState(null);
+
+  const fetchAddressData =  async ()=>{
+      const baseURL= "/api/address"
+      const data = await axios.get(baseURL, {
+        params:{
+          street:"8805 Sylvania St",
+          city:"Lorton",
+          state:"VA",
+          zip:"22079",
+          benchmark:"2020",
+          format:"json", 
+        }
+      })
+      console.log("data",data.data.result)
+  }
+
+  const handleAddressSubmit = ()=>{
+    fetchAddressData()
+  }
   return (
-    <main className={styles.main}>
-      <h1>Hello World</h1>
-      <div className={styles.center}>
-        <Image
-            className={styles.logo}
-            src="/construction.png"
-            alt="under construction image"
-            width={600}
-            height={600}
-            priority
-        />
-      </div>
-      <p>We're hard at work churning through data for DATA JAM 2023</p>
-    </main>
+    <Box>
+      <AppBar position="static" sx={{padding: "5px"}}>
+      <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            DATAJAM
+          </Typography>
+      </AppBar>
+      <Paper sx={{padding: "5px", height: "100vh"}}>
+      <TextField 
+        id="outlined-basic" 
+        label="Address" 
+        variant="outlined"  
+        sx={{margin: "10px 0px", width:"500px"}}
+        onChange={(input)=> {setAddress(input.target.value)}}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={handleAddressSubmit}>
+                <LocationSearchingIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+        <Map
+            initialViewState={{
+              longitude: -122.4,
+              latitude: 37.8,
+              zoom: 14
+            }}
+            style={{width: 500, height: 500}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={"pk.eyJ1IjoicXVhbm5ndXllbnN1bW1pdCIsImEiOiJjbGczdjRxb3MwZXEwM2VzYTBmOG53ankwIn0.3Z2bGiao8TQWuEhojfDBfQ"}
+          />
+      </Paper>
+      
+    </Box>
+      
   )
 }
