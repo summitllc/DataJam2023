401e03f98ee00ef85b61b7531831563ccb5c5331
Kyle Follendorf <kyle.follendorf@summitllc.us>
2023-04-22 11:28:29 +0100

    First half of test data parsed into array

--- data-jam-2023/app/data/facility-test-data.ts
+++ data-jam-2023/app/data/facility-test-data.ts
@@ -1,24 +1,208 @@
 interface Facility {
-    facilityName: string;
-    address: string;
-    contactInformation: string;
+  name: string | null;
+  name1: string | null;
+  name2: string | null;
+  street1: string | null;
+  street2: string | null;
+  city: string | null;
+  state: string | null;
+  zip: string | null;
+  zip4: string | null;
+  county: string | null;
+  phone: string | null;
+  intake_prompt: string | null;
+  intake1: string | null;
+  intake2: string | null;
+  website: string | null;
+  latitude: string | null;
+  longitude: string | null;
+  type_facility: string | null;
+  mh: string | null;
+  psy: string | null;
+  rtcc: string | null;
+  rtca: string | null;
+  ores: string | null;
+  ph: string | null;
+  omh: string | null;
+  cmhc: string | null;
+  msmh: string | null;
+  ipt: string | null;
+  cft: string | null;
+  gt: string | null;
+  cbt: string | null;
+  dbt: string | null;
+  idd: string | null;
+  at: string | null;
+  ect: string | null;
+  tele: string | null;
+  smon: string | null;
+  smpd: string | null;
+  smop: string | null;
+  hi: string | null;
+  res: string | null;
+  phdt: string | null;
+  op: string | null;
+  vamc: string | null;
+  tbg: string | null;
+  ih: string | null;
+  sf: string | null;
+  md: string | null;
+  mc: string | null;
+  si: string | null;
+  pi: string | null;
+  mi: string | null;
+  smha: string | null;
+  swfs: string | null;
+  scjj: string | null;
+  sef: string | null;
+  osf: string | null;
+  clf: string | null;
+  csbg: string | null;
+  cmhg: string | null;
+  vaf: string | null;
+  cit: string | null;
+  wi: string | null;
+  ss: string | null;
+  pa: string | null;
+  ah: string | null;
+  sp: string | null;
+  co: string | null;
+  gl: string | null;
+  vet: string | null;
+  adm: string | null;
+  mf: string | null;
+  cj: string | null;
+  se: string | null;
+  hv: string | null;
+  trma: string | null;
+  sed: string | null;
+  smi: string | null;
+  alz: string | null;
+  ptsd: string | null;
+  tbi: string | null;
+  tay: string | null;
+  ped: string | null;
+  act: string | null;
+  icm: string | null;
+  cm: string | null;
+  cdm: string | null;
+  peer: string | null;
+  coot: string | null;
+  dec: string | null;
+  es: string | null;
+  fpsy: string | null;
+  hs: string | null;
+  imr: string | null;
+  ipc: string | null;
+  lad: string | null;
+  nrt: string | null;
+  nsc: string | null;
+  prs: string | null;
+  
+  
   }
   
   const facilityTestData: Facility[] = [
     {
-      facilityName: "Facility Name Test 1",
-      address: "Facility Fake Address Test 1",
-      contactInformation: "Fake Phone Number Test 1"
-    },
-    {
-      facilityName: "Facility Name Test 2",
-      address: "Facility Fake Address Test 2",
-      contactInformation: "Fake Phone Number Test 2"
-    },
-    {
-      facilityName: "Facility Name Test 3",
-      address: "Facility Fake Address Test 3",
-      contactInformation: "Fake Phone Number Test 3"
+      name: "So Others Might Eat",
+      name1: "So Others Might Eat",
+      name2: null,
+      street1: "60 O Street NW",
+      street2: null,
+      city: "Washington",
+      state: "DC",
+      zip: "20001",
+      zip4: null,
+      county: "District of Columbia",
+      phone: "202-797-8806",
+      intake_prompt: null,
+      intake1: null,
+      intake2: null,
+      website: "http://www.some.org/",
+      latitude: "38.9082416",
+      longitude: "-77.0110598",
+      type_facility: "MH",
+      mh: "1.0",
+      psy: null,
+      rtcc: null,
+      rtca: null,
+      ores: null,
+      ph: null,
+      omh: "1.0",
+      cmhc: null,
+      msmh: null,
+      ipt: "1.0",
+      cft: null,
+      gt: "1.0",
+      cbt: "1.0",
+      dbt: null,
+      idd: null,
+      at: null,
+      ect: null,
+      tele: "1.0",
+      smon: null,
+      smpd: "1.0",
+      smop: null,
+      hi: null,
+      res: null,
+      phdt: null,
+      op: "1.0",
+      vamc: null,
+      tbg: null,
+      ih: null,
+      sf: null,
+      md: "1.0",
+      mc: "1.0",
+      si: "1.0",
+      pi: null,
+      mi: null,
+      smha: null,
+      swfs: null,
+      scjj: null,
+      sef: null,
+      osf: null,
+      clf: null,
+      csbg: null,
+      cmhg: null,
+      vaf: null,
+      cit: null,
+      wi: null,
+      ss: "1.0",
+      pa: "1.0",
+      ah: "1.0",
+      sp: null,
+      co: "1.0",
+      gl: null,
+      vet: null,
+      adm: null,
+      mf: null,
+      cj: null,
+      se: null,
+      hv: null,
+      trma: "1.0",
+      sed: null,
+      smi: null,
+      alz: null,
+      ptsd: "1.0",
+      tbi: null,
+      tay: null,
+      ped: null,
+      act: null,
+      icm: null,
+      cm: "1.0",
+      cdm: "1.0",
+      peer: null,
+      coot: null,
+      dec: "1.0",
+      es: null,
+      fpsy: null,
+      hs: null,
+      imr: null,
+      ipc: "1.0",
+      lad: null,
+      nrt: "1.0",
+      nsc: "1.0",
+      prs: null,
     }
   ];
   
