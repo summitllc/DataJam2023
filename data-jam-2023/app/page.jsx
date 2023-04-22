aac10a2f78f686586c9031eab7f2d80e0ca749f8
Kyle Follendorf <kyle.follendorf@summitllc.us>
2023-04-22 18:03:23 +0100

    Remove test log

--- data-jam-2023/app/page.jsx
+++ data-jam-2023/app/page.jsx
@@ -53,9 +53,6 @@ export default function Home() {
     };
 
     const facility = facilityTestData[currentIndex];
-    let i= 0;
-    Object.keys(facility).forEach(e => console.log(i++));
-
 
     useEffect(() => {
         console.log(showWhyDialog)
